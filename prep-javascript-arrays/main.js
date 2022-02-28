536ef5495d7051590744ad64001f2e5a80550c51
Louisa Whitaker <louisa.whitaker@gmail.com>
2022-02-28 20:34:37 +0000

    Completed prep-javascript-arrays exercise.

---
+++ prep-javascript-arrays/main.js
@@ -0,0 +1,16 @@
+const languages = [
+  'html',
+  'css',
+  'javascript',
+  'react',
+  'node',
+  'postgres'];
+console.log(languages);
+const firstElement = languages[0];
+console.log('The first element of the array is', firstElement);
+const thirdElement = languages[2];
+console.log('the third element of the array is', thirdElement);
+const length = languages.length;
+console.log(length);
+const lastElement = languages[languages.length - 1];
+console.log('the last element in the array is', lastElement);
